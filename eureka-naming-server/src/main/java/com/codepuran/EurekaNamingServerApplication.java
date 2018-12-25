786408a654fc4ebffea1be22e688b03459aafa19
Akshay Pethani <patelaksh412@gmail.com>
2018-12-26 01:10:04 +0530

    Load balancing with Eureka naming server and Zuul API gateway.

--- eureka-naming-server/src/main/java/com/codepuran/EurekaNamingServerApplication.java
+++ eureka-naming-server/src/main/java/com/codepuran/EurekaNamingServerApplication.java
@@ -2,15 +2,14 @@ package com.codepuran;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class StudentApplication {
+@EnableEurekaServer
+public class EurekaNamingServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StudentApplication.class, args);
+		SpringApplication.run(EurekaNamingServerApplication.class, args);
 	}
-
 }
 
--- zuul-api-gateway/src/main/java/com/codepuran/ZuulApiGatewayApplication.java
+++ zuul-api-gateway/src/main/java/com/codepuran/ZuulApiGatewayApplication.java
@@ -3,13 +3,15 @@ package com.codepuran;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 @EnableDiscoveryClient
-public class StudentApplication {
+public class ZuulApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StudentApplication.class, args);
+		SpringApplication.run(ZuulApiGatewayApplication.class, args);
 	}
 
 }
